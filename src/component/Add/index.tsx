7d20a511e85999e69779b49e493aaf88deb750ab
sheraz khan <sheraz.0403889@talmeez.pk>
2022-04-08 00:43:57 +0500

    card done

--- src/component/Add/index.tsx
+++ src/component/Add/index.tsx
@@ -1,27 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Add = () => {
-  return (
-    <div className='AddToList'>
-      <input
-        type='text'
-        placeholder='Enter Name..'
-        className='.AddToList-input '
-      />
-      <input
-        type='text'
-        placeholder='Enter  image Url..'
-        className='.AddToList-input '
-      />     <input
-        type='number'
-        placeholder='Enter age..'
-        className='.AddToList-input '
-      />     <textarea
-        placeholder='Enter Note..'
-        className='.AddToList-input '
-      />
-    </div>
-  )
+  const [input, setInput] = useState({
+    name: '',
+    age: '',
+    url: '',
+    note: ''
+
+  })
+  const handleChange = (e:React.ChangeEvent<HTMLInputElement |HTMLTextAreaElement>) => {
+    setInput({   ...input,
+  [e.target.name]: e.target.value} )
+  }
+return (
+  <div className='AddToList'>
+    <input
+      type='text'
+      placeholder='Enter Name..'
+      className='AddToList-input '
+      value={input.name}
+      onChange={handleChange}
+      name='name'
+    />
+    <input
+      type='text'
+      placeholder='Enter  image Url..'
+      className='AddToList-input '
+      value={input.url}
+      onChange={handleChange}
+      name='url'
+
+
+
+    />     <input
+      type='number'
+      placeholder='Enter age..'
+      className='AddToList-input '
+      value={input.age}
+      onChange={handleChange}
+      name='age'
+
+
+
+    />   
+      <textarea
+      placeholder='Enter Note..'
+      className='AddToList-input '
+      value={input.note}
+      onChange={handleChange}
+      name='note'
+
+
+
+    />
+  </div>
+)
 }
 
 export default Add
