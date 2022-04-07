84c7e45408143dcedfa012edd21187a384d84b13
sheraz khan <sheraz.0403889@talmeez.pk>
2022-04-08 00:27:19 +0500

    card done

--- src/component/Add/index.tsx
+++ src/component/Add/index.tsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Add = () => {
   return (
-    <div>Add</div>
+    <div className='AddToList'>
+      <input
+        type='text'
+        placeholder='Enter Name..'
+        className='.AddToList-input '
+      />
+      <input
+        type='text'
+        placeholder='Enter  image Url..'
+        className='.AddToList-input '
+      />     <input
+        type='number'
+        placeholder='Enter age..'
+        className='.AddToList-input '
+      />     <textarea
+        placeholder='Enter Note..'
+        className='.AddToList-input '
+      />
+    </div>
   )
 }
 
--- src/component/List/List.tsx
+++ src/component/List/List.tsx
@@ -1,18 +1,33 @@
 import React from 'react'
 
-interface IProps{
-    people:{
-      
-        name:string,
-        url:string,
-        age:number,
-        note:string
+interface IProps {
+    people: {
+
+        name: string,
+        url: string,
+        age: number,
+        note: string
     }[]
-  }
-const List = (props:IProps) => {
-  return (
-    <div>List of people</div>
-  )
+}
+const List: React.FC<IProps> = ({ people }) => {
+    return (
+        <ul>
+            {people.map((user) => {
+                return (
+                  <li className='List'>
+                      <div className='List-header'>
+                          <img src={user.url} alt='person' className='List-img'/>
+                          <h2>{user.name}</h2>
+                          
+                      </div>
+                      <p>{user.age} year old</p>
+                      <p className='List-note'>{user.note}</p>
+
+                  </li>
+                )
+            })}
+        </ul>
+    )
 }
 
 export default List
